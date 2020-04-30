1cb22c2c622227d391153b9618fbfe3bc62d3dd2
kiselevgleb <kiselevgleb@gmail.com>
2020-04-30 13:48:47 +0300

    event-seach-menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,38 @@
 // TODO: write your code here
 import sum from './basic';
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default function sort(params, m) {
+  let tempMas = params;
+  let masFirst = [];
+  let mas = [];
+  let coin = 0;
+  m.forEach(e => {
+    for (const key in tempMas) {
+      const element = tempMas[key];
+      if (tempMas.hasOwnProperty(key)) {
+        if (e == key) {
+          masFirst.push({
+            key: key,
+            value: element
+          });
+          delete tempMas[key];
+        } else if (coin == 1) {
+          mas.push({
+            key: key,
+            value: element
+          });
+          delete tempMas[key];
+        }
+      }
+    }
+    coin++;
+  });
+  mas.sort(function (a, b) {
+    var akey = a.key.toLowerCase(),
+      bkey = b.key.toLowerCase();
+    if (akey < bkey) return -1;
+    if (akey > bkey) return 1;
+  });
+  const merged = masFirst.concat(mas);
+  return merged;
+}
